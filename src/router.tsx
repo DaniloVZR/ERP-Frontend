2c8b3183ca05ff671498507f551484e2d1efd5fa
Danilo Urrego <daniloud2809@gmail.com>
2025-02-08 16:22:10 -0500

    Feature: Protected Routes

--- src/router.tsx
+++ src/router.tsx
@@ -4,20 +4,26 @@ import Home from "./pages/Home"
 import { Login } from "./pages/Login"
 import { Register } from "./pages/Register"
 import { LandingPage } from "./pages/LandingPage"
+import { ProtectedRoute } from "./components/ProtectedRoute"
 import { ToastContainer } from "react-toastify"
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes >
-        <Route element={<Layout />}>
-          <Route path="/home" element={<Home />} />
+        {/* Private routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Layout />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
         </Route>
+
+        {/* Public routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<LandingPage />} />
       </Routes>
       <ToastContainer />
-    </BrowserRouter>
+    </BrowserRouter >
   )
 }
